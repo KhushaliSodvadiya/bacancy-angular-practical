2d540afea6dcb0ef9b01baacc8efb3d333bce388
Khushali Sodvadiya <khushali.sodvadiya1804@gmail.com>
2021-03-17 16:24:54 +0530

    Hot fix in touting file.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { AuthGuard } from './pages/auth/auth-guard/auth.guard';
 import { HomeComponent } from './pages/app/home/home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   {
     path: 'auth',
     loadChildren: () => import('./././pages/auth/auth.module').then((module) => module.AuthModule)
-  },
-  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  }
 ];
 
 @NgModule({
